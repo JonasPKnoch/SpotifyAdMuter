a6eddc6327bae62646acefbb5c6ecae254fff0aa
Jonas P Knochelmann <74168898+JonasPKnoch@users.noreply.github.com>
2021-06-05 19:32:19 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Media;
+
+namespace AdMuter
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Process spotifyProcess = GetProcessName("Spotify");
+            VolumeController.SetApplicationVolume(spotifyProcess.Id, 1f);
+            bool muted = false;
+
+            while (true)
+            {
+                spotifyProcess = GetProcessName("Spotify");
+                if (spotifyProcess.MainWindowTitle == "Advertisement")
+                {
+                    VolumeController.SetApplicationVolume(spotifyProcess.Id, 0f);
+                    muted = true;
+                } else if(muted)
+                {
+                    VolumeController.SetApplicationVolume(spotifyProcess.Id, 1f);
+                    muted = false;
+                }
+                Debug.Print($"Found {spotifyProcess.MainWindowTitle}");
+                Thread.Sleep(1000);
+            }
+
+            Debug.Print("Done");
+        }
+
+        public static Process GetProcessName(string name)
+        {
+            Process result = null;
+            foreach (var process in Process.GetProcesses())
+            {
+                if (process.ProcessName == name && !String.IsNullOrEmpty(process.MainWindowTitle))
+                    result = process;
+            }
+
+            if (result == null)
+                return null;
+
+            return result;
+        }
+    }
+}
